40ad0a9d0c77db2f52e171862a4e806a4719f376
DanteManriqueOrellano <relaxurbano@me.com>
2022-04-28 12:17:24 -0500

    fix proxy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { SheetService } from './sheet.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[SheetService],
 })
 export class AppComponent implements OnInit {
   title = 'sheetdbv9angular';
